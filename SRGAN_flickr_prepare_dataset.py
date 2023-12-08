d1ccc43d191b1d1fc3ee201e335770b361ec78b1
Kritika Acharya <kritikaacharya@Kritikas-MacBook-Air.local>
2023-12-08 19:05:12 +0545

    worked

---
+++ SRGAN_flickr_prepare_dataset.py
@@ -0,0 +1,45 @@
+# # https://youtu.be/nbRkLE2fiVI
+# https://youtu.be/1HqjPqNglPc
+"""
+
+
+Dataset from: http://press.liacs.nl/mirflickr/mirdownload.html
+
+Read high res. original images and save lower versions to be used for SRGAN.
+
+Here, we are resizing them to 128x128 that will be  used as HR images and 
+32x32 that will be used as LR images
+"""
+import os
+import cv2
+
+# Specify the input directory
+input_dir = "mirflickr"
+
+# Specify the output directories
+hr_output_dir = "hr_images"
+lr_output_dir = "lr_images"
+
+# Create output directories if they don't exist
+os.makedirs(hr_output_dir, exist_ok=True)
+os.makedirs(lr_output_dir, exist_ok=True)
+
+# Loop through images in the input directory
+for img in os.listdir(input_dir):
+    # Read the image
+    img_path = os.path.join(input_dir, img)
+    img_array = cv2.imread(img_path)
+    
+    # Resize the image for high resolution
+    hr_img_array = cv2.resize(img_array, (128, 128))
+
+    # Resize the image for low resolution
+    lr_img_array = cv2.resize(img_array, (32, 32))
+
+    # Save the high-resolution image
+    hr_img_path = os.path.join(hr_output_dir, img)
+    cv2.imwrite(hr_img_path, hr_img_array)
+
+    # Save the low-resolution image
+    lr_img_path = os.path.join(lr_output_dir, img)
+    cv2.imwrite(lr_img_path, lr_img_array)
